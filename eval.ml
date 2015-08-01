b295888d7a09a7e0395d340d78eb8b3cc8093d59
ikuehne <ikuehne617@gmail.com>
2015-08-01 14:28:21 -0700

    Revert "	modified:   eval.ml"
    This reverts commit e45d46d18a341334ab718d7914e20427a2d235e8.

--- eval.ml
+++ eval.ml
@@ -22,11 +22,11 @@ let rec eval_checked ast env =
       | Some v -> v in
 
    match ast with
-      | Check_ast.Unit -> Val_unit
-      | Check_ast.Bool b -> Val_bool b
-      | Check_ast.Char c -> Val_char c
-      | Check_ast.Int i -> Val_int i
-      | Check_ast.ID id -> handle_lookup id (lookup env id)
+      | Check_ast.Unit    -> Val_unit
+      | Check_ast.Bool b  -> Val_bool b
+      | Check_ast.Char c  -> Val_char c
+      | Check_ast.Int  i  -> Val_int i
+      | Check_ast.ID   id -> handle_lookup id (lookup env id)
       | Check_ast.Define (id, e) -> 
          let definition = eval_checked e env in
          add env id definition;
@@ -34,10 +34,10 @@ let rec eval_checked ast env =
       | Check_ast.If (test_e, then_e, else_e) -> 
          begin 
             match eval_checked test_e env with
-            | Val_bool true -> (eval_checked then_e env)
+            | Val_bool true  -> (eval_checked then_e env)
             | Val_bool false -> (eval_checked else_e env)
-            | x -> raise (Type_Error ("ID",
-                                      type_of_value x))
+            | x              -> raise (Type_Error ("ID",
+                                                   type_of_value x))
          end
       | Check_ast.Lambda (ids, exprs) -> Val_lambda (env, ids, exprs)
       | Check_ast.Apply (f, args) ->
@@ -45,7 +45,7 @@ let rec eval_checked ast env =
          let operands = List.map ~f:(fun x -> eval_checked x env) args in
          (* Evaluate the function. *)
          match eval_checked f env with
-            | Val_prim prim -> prim operands
+            | Val_prim   prim -> prim operands
             | (Val_lambda (env, ids, exprs)) as lambda ->
                   let new_env  = make (Some env) in
                   begin
@@ -53,14 +53,14 @@ let rec eval_checked ast env =
                         add_all new_env ids operands;
                      with Invalid_argument _ ->
                         raise (Invalid_Args (string_of_value lambda,
-                                             List.length ids,
-                                             List.length operands))
+                                             List.length     ids,
+                                             List.length     operands))
                   end;
                   eval_lambda new_env exprs
             | x -> raise (Syntax_Error ("Value "
                                       ^ (string_of_value x)
-                                      ^ "is not a function; "
-                                      ^ "cannot apply."))
+                                      ^ " is not a function;"
+                                      ^ " cannot apply."))
 
 let eval ast env =
    let checked_ast = Check_ast.check ast in
